2fe81bbbad15d5104719597984fc09065d4bc6ae
Ranjani <ranju86100@gmail.com>
2026-06-15 16:19:48 +0530

    Countries App

--- countries_app/src/App.jsx
+++ countries_app/src/App.jsx
@@ -4,8 +4,8 @@ import { ErrorMessage } from "./UI_Comps/ErrorMessage.jsx";
 import { Loader } from "./UI_Comps/Loader.jsx";
 import { CountryList } from "./UI_Comps/CountryList.jsx";
 import { SearchBar } from "./UI_Comps/SearchBar.jsx";
-import { DetailsRow } from "./UI_Comps/DetailsRow.jsx";
 import { InitialCard } from "./UI_Comps/InitialCard.jsx";
+import { CountryDetails } from "./CountryDetails.jsx";
 
 export default function App() {
   const [searchCountry, setSearchCountry] = useState("");
@@ -147,51 +147,6 @@ export const CountryCard = ({
   `}
       >
         {<CountryDetails selectedCountry={country} />}
-      </div>
-    </>
-  );
-};
-
-const CountryDetails = ({ selectedCountry }) => {
-  if (!selectedCountry) return;
-  const currenciesArray = Object.values(selectedCountry.currencies);
-  const currencies = currenciesArray.map(
-    (currency) => `${currency.name} (${currency.symbol})`,
-  );
-  const borders =
-    selectedCountry.borders.length === 0 ? (
-      <p className="text-xl font-semibold">None</p>
-    ) : (
-      selectedCountry.borders.map((border) => `[${border}]`)
-    );
-  return (
-    <>
-      <div className="w-full max-w-2xl bg-blue-50 rounded-xl p-6 shadow-inner border border-blue-200">
-        <div className="grid grid-cols-2 gap-6 font-body">
-          <DetailsRow label="🌎 Subregion" value={selectedCountry.subregion} />
-          <DetailsRow
-            label="🗣️ Languages"
-            value={Object.values(selectedCountry.languages).join(", ")}
-          />
-          <DetailsRow label="💰 Currency" value={currencies} />
-
-          <DetailsRow>
-            <p className="font-bold font-heading text-lg text-blue-700">
-              🌐 Borders
-            </p>
-            <div className="flex flex-wrap gap-2 mt-1">
-              {<span className="text-cyan-500">{borders}</span>}
-            </div>
-          </DetailsRow>
-        </div>
-
-        <div className="mt-6 flex justify-center mr-10">
-          <img
-            src={selectedCountry.flags.png}
-            alt={`${selectedCountry.name.common} flag`}
-            className="w-40 rounded shadow"
-          />
-        </div>
       </div>
     </>
   );
